595a992f4a9348c785c779f346343a449d4b9d4e
Alexis <klementsingamesstudio@gmail.com>
2017-06-10 21:13:49 -0400

    tried to deploy to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606234407) do
+ActiveRecord::Schema.define(version: 20170609213256) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 20170606234407) do
     t.index ["followed_id"], name: "index_relationships_on_followed_id"
     t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
